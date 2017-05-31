4f3ccc31220abdf23fb30225c5c53f762f138a7a
Jabez Winston <jabezwinston@gmail.com>
2017-05-31 10:48:04 +0530

    Add comments to blinky.c

--- blinky.c
+++ blinky.c
@@ -1,16 +1,27 @@
+/*
+ * Blinks LED on connected to pin 13 (PB5 on Atmega328P)
+ * Have a look at ATmega328_ArduinoUno_pinmap.jpg
+ *
+ * Authors : Jabez Winston C, Hariharan K
+ *
+ */
+
 #define F_CPU 16e6  // Arduino Uno's ATmega328P runs at 16MHz 
 
 #include<avr/io.h>
 #include<util/delay.h>
+
 void main()
 {
-DDRB|=(1<<5);
+DDRB|=(1<<5); //Set PB5 as output by setting 5th bit DDRB register  
 
 	while(1)
 	{
-	PORTB|=(1<<5);
-	_delay_ms(100);
-	PORTB&=~(1<<5);
-	_delay_ms(100);
+	PORTB|=(1<<5);  //Set pin 13 (PB5)- High
+	_delay_ms(100); //Wait for 100 ms
+	PORTB&=~(1<<5); //Set pin 13 (PB5)- Low
+	_delay_ms(100); //Wait for 100ms
 	}
+	
+//Refer Atmega328P datasheet for details regarding DDRB and PORTB
 }
